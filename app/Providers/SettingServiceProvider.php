b81d2cfca58b3aebe887b3b5041275cd4e41effc
awanjila <abemuchikan@gmail.com>
2020-05-13 10:28:29 +0300

    autoloading all setting on project boot

--- app/Providers/SettingServiceProvider.php
+++ app/Providers/SettingServiceProvider.php
@@ -21,12 +21,16 @@ class SettingServiceProvider extends ServiceProvider
 }
 
     /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+ * Register services.
+ *
+ * @return void
+ */
+public function register()
+{
+    $this->app->bind('settings', function ($app) {
+        return new Setting();
+    });
+    $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+    $loader->alias('Setting', Setting::class);
+}
 }
