b61b7ce593f6ac0646a2e6236b0b62b79c57a6d2
awanjila <abemuchikan@gmail.com>
2020-05-13 10:23:13 +0300

    registering seting model as a facade

---
+++ app/Providers/SettingServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class SettingServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    $this->app->bind('settings', function ($app) {
+        return new Setting();
+    });
+    $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+    $loader->alias('Setting', Setting::class);
+}
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
