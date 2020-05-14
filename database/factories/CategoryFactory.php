6b32aab18090fef269ce7b7f7dd0ebc6cb450df4
awanjila <abemuchikan@gmail.com>
2020-05-14 11:29:40 +0300

    created category model and iration

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\AppModelsCategory;
+use Faker\Generator as Faker;
+
+$factory->define(AppModelsCategory::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
