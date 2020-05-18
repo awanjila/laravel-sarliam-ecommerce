bcf7022f90f65c59cebf2f33435e4279bf4e4317
awanjila <abemuchikan@gmail.com>
2020-05-19 00:51:00 +0300

    Laravel migration: “Foreign key constraint is incorrectly formed” (errno 150) Solved

--- database/migrations/2020_05_18_104217_create_product_categories_table.php
+++ database/migrations/2020_05_18_104217_create_product_categories_table.php
@@ -15,9 +15,9 @@ class CreateProductCategoriesTable extends Migration
     {
         Schema::create('product_categories', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('category_id')->unsigned()->index();
+            $table->unsignedBiginteger('category_id')->index();
             $table->foreign('category_id')->references('id')->on('categories');
-            $table->integer('product_id')->unsigned()->index();
+            $table->unsignedBiginteger('product_id')->index();
             $table->foreign('product_id')->references('id')->on('products');
         });
     }
