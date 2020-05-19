2c259fb200e4236a1244f58453624ecbf2842c18
awanjila <abemuchikan@gmail.com>
2020-05-19 23:30:37 +0300

    adding user login and register options o the site

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -42,20 +42,20 @@ class RegisterController extends Controller
     }
 
     /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
-    }
-
+ * Get a validator for an incoming registration request.
+ *
+ * @param  array  $data
+ * @return \Illuminate\Contracts\Validation\Validator
+ */
+protected function validator(array $data)
+{
+    return Validator::make($data, [
+        'first_name' => ['required', 'string', 'max:255'],
+        'last_name' => ['required', 'string', 'max:255'],
+        'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+        'password' => ['required', 'string', 'min:8', 'confirmed'],
+    ]);
+}
     /**
      * Create a new user instance after a valid registration.
      *
@@ -63,11 +63,15 @@ class RegisterController extends Controller
      * @return \App\User
      */
     protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
-    }
+{
+    return User::create([
+        'first_name' => $data['first_name'],
+        'last_name' => $data['last_name'],
+        'email' => $data['email'],
+        'password' => Hash::make($data['password']),
+        'address' => $data['address'],
+        'city' => $data['city'],
+        'country' => $data['country'],
+    ]);
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ require 'admin.php';
 |
 */
 
-Route::get('/', function () {
-	return view('welcome');
-});
+Route::view('/', 'site.pages.homepage');
 
 
 Auth::routes();
